0bc694ad2cf212c87ae4db9677571a9e583279fe
Coby Oram <cobyoram@gmail.com>
2020-02-13 02:41:47 -0700

    Working stuff. Categorical subplots in process

--- ds_useful.py
+++ ds_useful.py
@@ -24,8 +24,8 @@ def auto_subplots(df, **kwargs):
 
     '''
     EACH_SIZE = 3
-    WSPACE = .3
-    HSPACE = .7
+    # WSPACE = .3
+    # HSPACE = .7
     DEFAULT_BOXPLOT_WHIS = 1.5
 
     columns = df.select_dtypes(include='number').columns
@@ -33,7 +33,7 @@ def auto_subplots(df, **kwargs):
 
     if kwargs.get('limitx'):
         limitx = kwargs.get('limitx')
-        count_dimensions = tuple([limitx, int(count_dimensions/limitx + 1)])
+        count_dimensions = tuple([limitx, int(len_cols/limitx + 1)])
 
     else:
         try_num = len_cols
@@ -47,8 +47,8 @@ def auto_subplots(df, **kwargs):
     dimensions = tuple([count_dimensions[0] * EACH_SIZE, count_dimensions[1] * EACH_SIZE])
     plt.figure(figsize=dimensions)
 
-    for i, col in enumerate(columns):
-        plt.subplot(count_dimensions[0], count_dimensions[1], i+1)
+    for i, col in enumerate(columns, 1):
+        plt.subplot(count_dimensions[0], count_dimensions[1], i)
         if kwargs.get('kind'):
             kind = kwargs.get('kind')
             selection = ['hist', 'boxplot', 'swarm']
@@ -67,7 +67,8 @@ def auto_subplots(df, **kwargs):
             plt.hist(df[col])
         plt.title(col)
 
-    plt.subplots_adjust(wspace=WSPACE, hspace=HSPACE)
+    # plt.subplots_adjust(wspace=WSPACE, hspace=HSPACE)
+    plt.tight_layout()
     plt.show()
 # ------- END OF DISTRIBUTION SELF_MADE FUNCS ----------------------|
 
@@ -156,3 +157,93 @@ def get_percentiles(df, column_name, threshold):
     
     return min_percentile, max_percentile
 # ------- END OF OUTLIER SELF_MADE FUNCS ----------------------|
+
+# ----- FUNCTIONS FOR GENERAL OUTLIER HANDLING --------------|
+
+# ------- END OF OUTLIER SELF_MADE FUNCS ----------------------|
+
+
+
+
+
+
+
+
+# def auto_subplots(df, **kwargs):
+#     '''
+#     This function creates a series of sublots to display the distribution for all continuous variables in a DataFrame
+#     It operates like a FacetGrid, but it's a little more customized.
+
+#     The dimensions of the subplot (no_subplots X no_subplots) are calculated automatically by how many continuous variables are found.
+#     You can use a number of kwargs to customize the output of the function. Those include:
+#     kwargs:
+#         limitx  -   Limits the number of subplots along the x-axis, and calculates the no_subplots on y axis from given limit
+#         kind    -   Allows you to specify which type of distribution grid you'd like to use. Values include
+#             -   hist: creates a matplotlib.pyplot histogram
+#             -   boxplot: creates a seaborn boxplot
+#                 -   whis: adjust the boxplot whisker bounds
+#             -   swarm: create a one-dimensional seaborn swarmplot
+
+#     Returns None
+
+#     '''
+#     EACH_SIZE = 3
+#     WSPACE = .3
+#     HSPACE = .7
+#     DEFAULT_BOXPLOT_WHIS = 1.5
+
+#     columns = df.select_dtypes(include='number').columns
+#     len_cols = len(columns)
+
+#     categories = df.select_dtypes(include='object').columns
+#     len_cats = len(categories)
+
+#     if kwargs.get('limitx'):
+#         limitx = kwargs.get('limitx')
+#         count_dimensions = tuple([limitx, int(len_cols/limitx + 1)])
+#     else:
+#         try_num = len_cols
+#         while True:
+#             sq = math.sqrt(try_num)
+#             if sq == int(sq):
+#                 break
+#             try_num += 1
+#         count_dimensions = tuple([sq, sq])
+
+#     dimensions = tuple([count_dimensions[0] * EACH_SIZE, count_dimensions[1] * EACH_SIZE])
+#     plt.figure(figsize=dimensions)
+
+#     for category in categories:
+#         for sub_cat in df[category].unique():
+#             if not kwargs.get('categorical'):
+#                 sub_cat = df[category].unique()
+
+#             plot_df = df.loc[df[category].isin(pd.Series(sub_cat))]
+
+#             for i, col in enumerate(columns, 1):
+#                 plt.subplot(count_dimensions[0], count_dimensions[1], i)
+#                 if kwargs.get('kind'):
+#                     kind = kwargs.get('kind')
+#                     selection = ['hist', 'boxplot', 'swarm']
+#                     if kind == 'hist':
+#                         plt.hist(plot_df[col])
+#                     elif kind == 'boxplot':
+#                         whis = DEFAULT_BOXPLOT_WHIS
+#                         if kwargs.get('whis'):
+#                             whis = kwargs.get('whis')
+#                         sns.boxplot(plot_df[col], whis=whis)
+#                     elif kind == 'swarm':
+#                         sns.swarmplot(y=col, data=plot_df)
+#                     else:
+#                         print('Kind: {} is currently unavailable. For now enjoy our limited selection of: {}'.format(kind, selection))
+#                 else:
+#                     plt.hist(plot_df[col])
+#             plt.title(col)
+#         if not kwargs.get('categorical'):
+#             break
+
+#     # plt.subplots_adjust(wspace=WSPACE, hspace=HSPACE)
+#     plt.tight_layout()
+#     plt.show()
+
+# auto_subplots(life_df, categorical=True)
