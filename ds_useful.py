b7e0c0619cf61b6c9ebca51696354f0cf149db11
Coby Oram <cobyoram@gmail.com>
2020-02-19 22:38:54 -0700

    fixed limitx calculation

--- ds_useful.py
+++ ds_useful.py
@@ -91,7 +91,7 @@ def make_subplots(df, plotfunc=None, func_args=[], func_kwargs={}, limitx=8, eac
     count_dimensions = tuple([sq, sq])
 
     if count_dimensions[0] > limitx:
-        count_dimensions = tuple([limitx, int(len_cols/limitx + 1)])
+        count_dimensions = tuple([int(len_cols/limitx) + 1], limitx)
 
     dimensions = tuple([count_dimensions[0] * each_size, count_dimensions[1] * each_size])
     plt.figure(figsize=dimensions)
